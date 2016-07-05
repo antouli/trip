09abc9e21ce105fe4e692b9f149cb38532dd3d6e
antouli <antouli@163.com>
2016-07-05 16:15:53 +0800

    first

---
+++ src/main/java/com/tqy/trip/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.tqy.trip.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2016/7/5.
+ */
+@Controller
+public class TestController {
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>trip go on</h2>
+</body>
+</html>
