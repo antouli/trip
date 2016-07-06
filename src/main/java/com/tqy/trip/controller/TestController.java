fa446ac865426d751cd8d46d117fcc0aed5e812d
tangqinyang <antouli@163.com>
2016-07-06 15:06:57 +0800

    commit

--- src/main/java/com/tqy/trip/controller/TestController.java
+++ src/main/java/com/tqy/trip/controller/TestController.java
@@ -1,10 +1,19 @@
 package com.tqy.trip.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2016/7/5.
  */
 @Controller
+@RequestMapping("/test")
 public class TestController {
+
+    @RequestMapping("/test1")
+    @ResponseBody
+    public String test1(){
+        return "test1";
+    }
 }
